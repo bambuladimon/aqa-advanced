0154034f299b2d789e95a0f0649aaad2a928219e
Dmytro Kulikovskyi <bambula.dimon@gmail.com>
2025-10-13 19:56:27 +0300

    Lessons 5 - HW - work with conditions and loops

---
+++ lessons5/task2.js
@@ -0,0 +1,25 @@
+let averageGrade = 85;
+let result;
+
+switch (true) {
+  case (averageGrade  < 60):
+    result = "Незадовільно";
+    break;
+  case (averageGrade >= 60 && averageGrade <= 70):
+    result = "Задовільно";
+    break;
+  case (averageGrade >= 71 && averageGrade <= 80):
+    result = "Добре";
+    break;
+  case (averageGrade >= 81 && averageGrade <= 90):
+    result = "Дуже добре";
+    break;
+  case (averageGrade >= 91 && averageGrade <= 100):
+    result = "Відмінно";
+    break;
+  default:
+    result = "Некоректне значення оцінки";
+}
+
+console.log("Середня оцінка:", averageGrade);
+console.log("Рівень успішності:", result);
