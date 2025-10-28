9cd3a6fa21c15aa0fd814c335f9bc1230c82abe1
Dmytro Kulikovskyi <bambula.dimon@gmail.com>
2025-10-28 11:50:16 +0200

    Add tasks with arrays from lessons 8

---
+++ lessons-8/task1.js
@@ -0,0 +1,19 @@
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8];
+let positiceNums = 0;
+let negativeNumbers = 0;
+let zeroNumbers = 0;
+
+for (let i = 0; i < numbers.length; ++i) {
+    switch (true) {
+        case (numbers[i] == 0):
+            zeroNumbers += 1;
+        case (numbers[i] > 0):
+            positiceNums += 1;
+        case (numbers[i] < 0):
+            negativeNumbers += 1;
+    }
+}
+
+console.log(`Кількість позитивних чисел: ${positiceNums}`);
+console.log(`Кількість негативних чисел: ${negativeNumbers}`);
+console.log(`Кількість нульових чисел: ${zeroNumbers}`);
---
+++ lessons-8/task4.js
@@ -0,0 +1,7 @@
+const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = arrNum.filter(function(number) {
+  return number % 2 === 0; // Повертає true, якщо число парне
+});
+
+console.log(evenNumbers);
