740b84910451ec463ea66eb53511998ead27466d
Dmytro Kulikovskyi <bambula.dimon@gmail.com>
2026-02-05 09:40:30 +0200

    Fix described problem

--- lessons-9/task5.js
+++ lessons-9/task5.js
@@ -1,11 +1,16 @@
-let users = [
-  { id: 1, name: "Анна" },
-  { id: 2, name: "Іван" },
-  { id: 4, name: "Юра" },
-  { id: 7, name: "Катя" }
+const users = [
+  { id: 1, name: "Анна", age: 25 },
+  { id: 2, email: "ivan@test.com" },
+  { id: 3, name: "Юра" },
+  { id: 4 },
+  { id: 5, name: "Катя", email: "katya@test.com" }
 ];
- 
-for (let user of users) {
-    console.log(user);
-    
+
+for (const { 
+  id, 
+  name = "Невідоме ім'я", 
+  email = "email відсутній", 
+  age = "вік не вказано" 
+} of users) {
+  console.log({ id, name, email, age });
 }
